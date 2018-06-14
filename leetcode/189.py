9533ce2f7d136a93b929108b2943f0d8aeb1b3e6
Qing-Cheng Li <qc.linux@gmail.com>
2018-06-14 22:00:12 +0200

    can not realize why there is problem like this one

---
+++ leetcode/189.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        for i in range(0,k):
+            nums.insert(0,nums.pop())
